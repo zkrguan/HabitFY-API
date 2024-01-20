99202b913a96176b6d4e24face740fd6db8a184f
roger1g <zguan25@myseneca.ca>
2024-01-20 12:21:25 -0500

    Adding some learning materials for the team

--- HabitFY/HabitFY/Program.cs
+++ HabitFY/HabitFY/Program.cs
@@ -1,3 +1,6 @@
+using HabitFY.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // RG's note: You are configring here whenever you add a new packages.
@@ -7,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// RG: Remember the DBcontext's lil contrusctor? 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    // RG: Now the connection string from the appsetting.json is used by EF to connect. 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
