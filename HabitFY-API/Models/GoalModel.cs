3cb5572ef425134798847b1449d59dabb64772ac
Zhaokai Guan <zguan25@myseneca.ca>
2024-02-07 11:56:04 -0500

    Models are initially set. Moving onto DB connection and testing CRUD in routes.

---
+++ HabitFY-API/Models/GoalModel.cs
@@ -0,0 +1,24 @@
+﻿namespace HabitFY_API.Models
+{
+    public class GoalModel
+    {
+        public int Id { get; set; }
+        public required string Description { get; set; }
+        public required DateTime StartDate { get; set; }
+        public required DateTime EndDate { get; set;}
+        public required DateTime LastUpdated { get; set; }
+        public required DateTime CreatedTime {  get; set; }    
+        // Is this became to an archive of the user.
+        public required bool IsActivated { get; set; }
+        public required bool IsQuitting { get; set; }
+        // RG: I am out of ideas on name convention, 
+        // Let me know if this is bad on the weekly meeting
+        public required double GoalValue { get; set; }
+        // One Profile can have many Goals
+        // One Goal can only be referred to one Profile
+        public required UserProfileModel Profile { get; set; }
+
+        // The goal could have one zero or multiple records
+        public ICollection<ProgressRecordModel>? ProgressRecords { get; set; }
+    }
+}
---
+++ HabitFY-API/Models/ProgressRecordModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HabitFY_API.Models
+{
+    public class ProgressRecordModel
+    {
+        public int Id { get; set; }
+
+        public required DateTime CreatedTime {  get; set; }
+
+        public string? Notes {  get; set; }
+
+        public required double CompletedValue {  get; set; }
+
+        // One record must relevant to one goal.
+        public required GoalModel Goal { get; set; }
+    }
+}
