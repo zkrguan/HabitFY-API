3f218fc83d31904164790664bde802235b761ab9
Zhaokai Guan <zguan25@myseneca.ca>
2024-02-21 10:33:40 -0500

    Hot fix. removed the cosmos service from the userprofile controller because test ended.

--- HabitFY-API/Services/UserProfileService.cs
+++ HabitFY-API/Services/UserProfileService.cs
@@ -14,13 +14,11 @@ namespace HabitFY_API.Services
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        private readonly CosmosService _cosmosService;
 
-        public UserProfileService(IUnitOfWork unitOfWork,IMapper mapper,CosmosService cosmosService)
+        public UserProfileService(IUnitOfWork unitOfWork,IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
-            _cosmosService = cosmosService;
         }
         
         // So far we only need like 
@@ -48,30 +46,9 @@ namespace HabitFY_API.Services
             _unitOfWork.Save();
         }
 
-
-        // RG: Don't touch, I am doing Nuke Testing with Mr.Kim here.
-        // public async Task<UserReminderSetting> TestService()
         public async Task TestService()
 
         {
-            //var userProfiles = new List<UserProfile>
-            //{
-            //    new UserProfile { Id = "user1", NeedReport = true, Sex = "Male", Province = "Ontario", City = "Toronto", PostalCode = "M1X 2Y4", Age = 25 },
-            //    new UserProfile { Id = "user2", NeedReport = false, Sex = "Female", Province = "Quebec", City = "Montreal", PostalCode = "H1A 1A1", Age = 30 },
-            //    new UserProfile { Id = "user3", NeedReport = true, Sex = "Male", Province = "British Columbia", City = "Vancouver", PostalCode = "V6Z 2C1", Age = 40 },
-            //    new UserProfile { Id = "user4", NeedReport = true, Sex = "Female", Province = "Alberta", City = "Calgary", PostalCode = "T2P 2V7", Age = 35 },
-            //    new UserProfile { Id = "user5", NeedReport = false, Sex = "Male", Province = "Manitoba", City = "Winnipeg", PostalCode = "R3C 1S3", Age = 28 },
-            //    new UserProfile { Id = "user6", NeedReport = true, Sex = "Female", Province = "Saskatchewan", City = "Regina", PostalCode = "S4P 3C8", Age = 45 },
-            //    new UserProfile { Id = "user7", NeedReport = false, Sex = "Male", Province = "Nova Scotia", City = "Halifax", PostalCode = "B3H 4R2", Age = 32 },
-            //    new UserProfile { Id = "user8", NeedReport = true, Sex = "Female", Province = "New Brunswick", City = "Fredericton", PostalCode = "E3B 5A3", Age = 38 },
-            //    new UserProfile { Id = "user9", NeedReport = false, Sex = "Male", Province = "Prince Edward Island", City = "Charlottetown", PostalCode = "C1A 4P3", Age = 27 },
-            //    new UserProfile { Id = "user10", NeedReport = true, Sex = "Female", Province = "Newfoundland and Labrador", City = "St. John's", PostalCode = "A1B 4J6", Age = 42 }
-            //};
-
-            //// RG: Boys, this is a perfect example of the difference between UOW and REPO
-            //_unitOfWork.UserProfile.AddRange(userProfiles); // Add the user profile to the repository
-            //_unitOfWork.Save(); // Save changes to the database
-
 
             // _________________test creation using cosmos
             //var obj = new UserReminderSetting { id = Guid.NewGuid(), UserId = "2sdsa3F", RemindInterval = 300 };
@@ -83,11 +60,12 @@ namespace HabitFY_API.Services
 
             //// _________________test get using cosmos
             ///
-            // _________________test update using cosmos
-            var updateObj = new UserReminderSetting { id = new Guid("5c31cdf6-dd97-472d-ae47-218c198d1a78"), UserId = "2sdsa3F", RemindInterval = 200 };
+            //// _________________test update using cosmos
+            //var updateObj = new UserReminderSetting { id = new Guid("5c31cdf6-dd97-472d-ae47-218c198d1a78"), UserId = "2sdsa3F", RemindInterval = 200 };
 
-            await _cosmosService.UpdateSettingAsync("2sdsa3F", updateObj);
-            // _________________test update using cosmos
+            //await _cosmosService.UpdateSettingAsync("2sdsa3F", updateObj);
+            //// _________________test update using cosmos
+            Console.WriteLine("wait");
         }
     }
 }
