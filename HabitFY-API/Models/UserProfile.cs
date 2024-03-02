e97144f5c8a51b18c7f2bac2d5555780239b6f81
Zhaokai Guan <zguan25@myseneca.ca>
2024-03-02 18:50:13 -0500

    adding migrations for email adress user profile logic

--- HabitFY-API/Models/UserProfile.cs
+++ HabitFY-API/Models/UserProfile.cs
@@ -15,9 +15,9 @@ namespace HabitFY_API.Models
         // RG: Frontend people -> while you sending the token, also send the user_id over.
         // This is the ID from the AWS hash Cognito ID pool
         // Which should be attached to the req.header 
-
         public required string Id { get; set; }
         public required bool NeedReport { get; set; }
+        public required string EmailAddress { get; set; }
         // Reminder:
         // RG to Frontend -> https://cihr-irsc.gc.ca/e/48642.html
         // Give the user a description about what is sex so beofre they got offended.
